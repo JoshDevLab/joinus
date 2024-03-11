0b78683a44b826ce2fb12408306b8717f438695f
josh <tkddns6007@gmail.com>
2024-03-11 14:38:45 +0900

    feat: ResponseEntity -> ApiResponse 반환값으로 변경

---
+++ core/core-api/src/main/java/com/josh/joinus/core/api/support/error/ErrorCode.java
@@ -0,0 +1,7 @@
+package com.josh.joinus.core.api.support.error;
+
+public enum ErrorCode {
+
+    E500
+
+}
---
+++ core/core-api/src/main/java/com/josh/joinus/core/api/support/response/ResultType.java
@@ -0,0 +1,7 @@
+package com.josh.joinus.core.api.support.response;
+
+public enum ResultType {
+
+    SUCCESS, ERROR
+
+}
